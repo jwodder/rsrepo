3adeb7577f77f6c08919d8eee74530ae487e98db
John T. Wodder II <git@varonathe.org>
2023-05-09 23:34:48 -0400

    Test & fix

--- src/changelog.rs
+++ src/changelog.rs
@@ -21,33 +21,35 @@ impl FromStr for Changelog {
 
     fn from_str(s: &str) -> Result<Changelog, ParseChangelogError> {
         let mut sections = Vec::new();
+        let mut current: Option<SectionBuilder> = None;
         let mut prev: Option<&str> = None;
         for line in s.lines() {
             if line.chars().all(|ch| ch == '-') && line.len() >= 3 {
+                if let Some(sb) = current.take() {
+                    sections.push(sb.build());
+                }
                 if let Some(p) = prev.take() {
-                    sections.push(ChangelogSection {
-                        header: p.parse::<ChangelogHeader>()?,
-                        content: String::new(),
-                    });
+                    current = Some(SectionBuilder::new(p.parse::<ChangelogHeader>()?));
                 } else {
                     return Err(ParseChangelogError::UnexpectedHrule);
                 }
             } else if let Some(p) = prev.replace(line) {
-                if let Some(sect) = sections.last_mut() {
-                    sect.content.push_str(p);
-                    sect.content.push('\n');
+                if let Some(sb) = current.as_mut() {
+                    sb.push_line(p);
                 } else {
                     return Err(ParseChangelogError::TextBeforeHeader);
                 }
             }
         }
         if let Some(p) = prev {
-            if let Some(sect) = sections.last_mut() {
-                sect.content.push_str(p);
-                sect.content.push('\n');
+            if let Some(sb) = current.as_mut() {
+                sb.push_line(p);
             } else {
                 return Err(ParseChangelogError::TextBeforeHeader);
             }
+        }
+        if let Some(sb) = current.take() {
+            sections.push(sb.build());
         }
         Ok(Changelog { sections })
     }
@@ -61,9 +63,9 @@ impl fmt::Display for Changelog {
             .map(|sect| sect.to_string())
             .collect::<Vec<_>>();
         let sep = if sections.iter().any(|sect| sect.contains("\n\n")) {
-            "\n\n\n"
-        } else {
             "\n\n"
+        } else {
+            "\n"
         };
         write!(f, "{}", sections.join(sep))?;
         Ok(())
@@ -130,6 +132,44 @@ pub enum ParseChangelogError {
 #[error("invalid changelog header title")]
 pub struct ParseHeaderError;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct SectionBuilder<'a> {
+    header: ChangelogHeader,
+    lines: Vec<&'a str>,
+}
+
+impl<'a> SectionBuilder<'a> {
+    fn new(header: ChangelogHeader) -> Self {
+        SectionBuilder {
+            header,
+            lines: Vec::new(),
+        }
+    }
+
+    fn push_line(&mut self, line: &'a str) {
+        self.lines.push(line);
+    }
+
+    fn build(mut self) -> ChangelogSection {
+        while let Some(line) = self.lines.last() {
+            if line.is_empty() {
+                self.lines.pop();
+            } else {
+                break;
+            }
+        }
+        let mut content = String::new();
+        for line in self.lines {
+            content.push_str(line);
+            content.push('\n');
+        }
+        ChangelogSection {
+            header: self.header,
+            content,
+        }
+    }
+}
+
 fn parse_header(input: &str) -> IResult<&str, ChangelogHeader> {
     alt((versioned_header, in_development))(input)
 }
@@ -166,4 +206,19 @@ fn in_development(input: &str) -> IResult<&str, ChangelogHeader> {
     tag_no_case("in development")
         .map(|_| ChangelogHeader::InDevelopment)
         .parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn changelog01() {
+        let src = include_str!("testdata/changelog01.md");
+        let jsonsrc = include_str!("testdata/changelog01.json");
+        let changelog = src.parse::<Changelog>().unwrap();
+        let expected = serde_json::from_str::<Changelog>(jsonsrc).unwrap();
+        assert_eq!(changelog, expected);
+        assert_eq!(changelog.to_string(), src);
+    }
 }
